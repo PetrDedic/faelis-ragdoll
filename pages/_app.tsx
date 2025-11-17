ba6b8d2052771c55954f784c9ca38b5511067cad
Petr Dědic <petandedic@gmail.com>
2025-11-17 03:44:21 +0100

    Add Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ import Footer from "../components/Footer";
 import "@mantine/dates/styles.css";
 import "@mantine/notifications/styles.css";
 import "../styles.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 // Initialize both fonts
 const roboto = Roboto({
@@ -22,42 +23,45 @@ const paytone = Paytone_One({
 
 export default function App({ Component, pageProps }: any) {
   return (
-    <MantineProvider
-      theme={{
-        ...theme,
-        fontFamily: roboto.style.fontFamily,
-        headings: {
-          // This will make headings use Paytone One by default
-          fontFamily: paytone.style.fontFamily,
-        },
-      }}
-    >
-      <Head>
-        <title>Faelis | Chovatelská stanice koček Ragdoll</title>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href="/favicon-96x96.png"
-          sizes="96x96"
-        />
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-      </Head>
-      <HeaderMenu />
-      <main>
-        <Component {...pageProps} />
-      </main>
-      <Footer />
-    </MantineProvider>
+    <>
+      <GoogleAnalytics gaId="G-H20VS0JX18" />
+      <MantineProvider
+        theme={{
+          ...theme,
+          fontFamily: roboto.style.fontFamily,
+          headings: {
+            // This will make headings use Paytone One by default
+            fontFamily: paytone.style.fontFamily,
+          },
+        }}
+      >
+        <Head>
+          <title>Faelis | Chovatelská stanice koček Ragdoll</title>
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            href="/favicon-96x96.png"
+            sizes="96x96"
+          />
+          <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+        </Head>
+        <HeaderMenu />
+        <main>
+          <Component {...pageProps} />
+        </main>
+        <Footer />
+      </MantineProvider>
+    </>
   );
 }
